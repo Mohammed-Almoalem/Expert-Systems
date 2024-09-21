feff3f1be35a66b8e09761efb3612cbef6ff71d3
Mohammed Almoalem <info.almoalem.co@gmail.com>
2024-09-21 17:34:03 +0300

    Create backward_chaining.py

---
+++ Home_Work_1/backward_chaining.py
@@ -0,0 +1,32 @@
+knowledge_base = {
+    'type' : {
+        'croaks&eats flies' : 'frog',
+        'chirps&sings' : 'canary'
+    },
+    'color' : {
+        'frog' : 'green',
+        'canary' : 'yellow'
+    }
+}
+
+facts = set()
+
+def answer(query:str, name:str) -> str:
+    # push first concludes
+    goals = set()
+    for key in knowledge_base[query]:
+        goals.add(key)
+    
+    # search in one iterate
+
+    for key in knowledge_base:
+        for key_ in knowledge_base[key]:
+            if key_ in facts and knowledge_base[key][key_] in goals:
+                facts.add(knowledge_base[key][key_])
+            if key == query and key_ in facts:
+                return(f'{name} {query} is {knowledge_base[key][key_]}')
+
+    return(f'{name} {query} is Unkown')
+
+facts.add('chirps&sings')
+print(answer('type', 'Fritz'))
