980bb4b91ebcc5c9dc563dd6beaece5d1267ca7d
Mohammed Almoalem <info.almoalem.co@gmail.com>
2024-09-21 17:32:54 +0300

    Create forward_chaining.py

---
+++ Home_Work_1/forward_chaining.py
@@ -0,0 +1,35 @@
+knowledge_base = {
+    'type' : {
+        'croaks&eats flies' : 'frog',
+        'chirps&sings' : 'canary'
+    },
+    'color' : {
+        'frog' : 'green',
+        'canary' : 'yellow'
+    },
+    'size' : {
+        'frog' : 'small',
+        'canary' : 'bigger'
+    }
+}
+
+facts = set()
+
+def answer(query:str, name:str) -> str:
+    
+    flage = True # for while loop
+    while flage:
+        flage = False
+        for key in knowledge_base:
+            for key_ in knowledge_base[key]:
+                if key_ in facts:
+                    flage = True
+                    facts.add(knowledge_base[key][key_])
+                    if query == key:
+                        return f'{name} {query} is {knowledge_base[key][key_]}'
+                    
+    return f'{name} {query} is Unknown'
+
+facts.add('croaks&eats flies')
+
+print(answer('color', 'Fritz'))
